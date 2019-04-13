2638e27694997b833881c497fc4d825096ca99cc
Armands <armands.avotins@gmail.com>
2019-04-13 17:17:17 +0300

    View for base operations created

--- Monetizr-v3-sample-app/Monetizr-v3-sample-app/ViewController.swift
+++ Monetizr-v3-sample-app/Monetizr-v3-sample-app/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var firstBlock: UIView!
+    @IBOutlet var secondBlock: UIView!
+    
+    @IBOutlet var tokenField: UITextField!
+    @IBOutlet var merchIdField: UITextField!
+    @IBOutlet var merchTagField: UITextField!
+    
+    @IBOutlet var openButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tokenField.text = "4D2E54389EB489966658DDD83E2D1"
+        merchIdField.text = "1794883780674"
+        merchTagField.text = "30-credits"
     }
 
 
