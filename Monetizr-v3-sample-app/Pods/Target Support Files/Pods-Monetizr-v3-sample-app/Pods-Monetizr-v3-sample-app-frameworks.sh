0d42c5a74f6c9316c6cf3b8b7bf4a866ca3525e3
Armands <armands.avotins@gmail.com>
2019-11-28 22:21:13 +0200

    Stripe POD removed

--- Monetizr-v3-sample-app/Pods/Target Support Files/Pods-Monetizr-v3-sample-app/Pods-Monetizr-v3-sample-app-frameworks.sh
+++ Monetizr-v3-sample-app/Pods/Target Support Files/Pods-Monetizr-v3-sample-app/Pods-Monetizr-v3-sample-app-frameworks.sh
@@ -166,7 +166,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ImageSlideshow/ImageSlideshow.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/McPicker/McPicker.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Mobile-Buy-SDK/MobileBuySDK.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Stripe/Stripe.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
@@ -174,7 +173,6 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ImageSlideshow/ImageSlideshow.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/McPicker/McPicker.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Mobile-Buy-SDK/MobileBuySDK.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Stripe/Stripe.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
